ea476aa03169ead56ae53a7a9b941037c98e1e43
Stanislav Moskalenko <maskal2719@gmail.com>
2023-03-09 14:26:39 +0300

    add 13 - controlled input, checkbox, select

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -2,6 +2,7 @@ import React, {useRef, useState} from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { within, userEvent } from '@storybook/testing-library';
 import { Page } from './Page';
+import {action} from "@storybook/addon-actions";
 
 export default {
   title: 'Input',
@@ -33,6 +34,35 @@ export const GetValueByBtnPressUncontrolledInput = () => {
   )
 }
 
+export const ControlledInput = () => {
+
+    const [parentValue, setParentValue] = useState('')
+
+    return (
+        <input value={parentValue} type="text" onChange={(e) => setParentValue(e.currentTarget.value)}/>
+    )
+}
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true)
+
+    return (
+        <input type="checkbox" checked={parentValue} onChange={(e) => setParentValue(e.currentTarget.checked)}/>
+    )
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>(undefined)
+
+    return (
+        <select value={parentValue} onChange={(e) => setParentValue(e.currentTarget.value)}>
+            <option >none</option>
+            <option value="1">Minsk</option>
+            <option value="2">Moscow</option>
+            <option value="3">Gomel</option>
+        </select>
+    )
+}
 
 export const ControlledInputWithFixedValue = () => <input value={1324123} />;
 
