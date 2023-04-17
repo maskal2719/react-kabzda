4d2ac5c1bbee65f90492139828936f6102746cc7
Stanislav Moskalenko <maskal2719@gmail.com>
2023-04-17 16:03:25 +0300

    22 UseEffect.stories.tsx (task clock)

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -17,18 +17,50 @@ export const Example1 = () => {
 
     useEffect(() => {
         console.log('useEffect work first render')
-    },[])
+    }, [])
 
     useEffect(() => {
         console.log('useEffect work first render and every count change')
         document.title = count.toString()
-    },[count])
+    }, [count])
 
     return (
         <>
             <div>
                 <h1>Hello {count}</h1>
                 <button onClick={() => setCount(count + 1)}>+</button>
+            </div>
+        </>
+    )
+}
+
+export const SetTimeOutExample2 = () => {
+
+    const [count, setCount] = useState(1)
+    const [clock, setClock] = useState(new Date())
+
+    console.log('Example')
+
+    useEffect(() => {
+
+        // setTimeout(() => {
+        //     document.title = count.toString()
+        // },1000)
+
+
+        setInterval(() => {
+            setClock(new Date())
+        }, 1000)
+
+    }, [])
+
+    return (
+        <>
+            <div>
+                <h1>Hello {count}</h1>
+                <div>
+                    {`${clock.getHours()}:${clock.getMinutes()}:${clock.getSeconds()}`}
+                </div>
             </div>
         </>
     )
